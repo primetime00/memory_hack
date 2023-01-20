57c06c81e14341c4083b4f10f7c734b1d078afd9
Ryan Kegel <ryan.kegel@gmail.com>
2023-01-20 17:00:21 -0500

    - fixes for windows

--- app/scripts/pointer/__init__.py
+++ app/scripts/pointer/__init__.py
@@ -8,4 +8,4 @@ from .pointer_verify_script import PointerVerify
 if platform.system() == 'Linux':
     from .pointer_scanner_helpers_linux import *
 else:
-    from .pointer_scanner_helpers_windows import *
+    from .pointer_scanner_helpers_windows import add_region, weigh_regions, get_node_bounds, is_heap_path
--- app/scripts/pointer/pointer_scanner_helpers.py
+++ app/scripts/pointer/pointer_scanner_helpers.py
@@ -1,5 +1,5 @@
 import platform
 if platform.system() == 'Linux':
-    pass
+    from .pointer_scanner_helpers_linux import add_region, is_heap_path, weigh_regions, get_node_bounds
 else:
-    pass
+    from .pointer_scanner_helpers_windows import add_region, is_heap_path, weigh_regions, get_node_bounds
